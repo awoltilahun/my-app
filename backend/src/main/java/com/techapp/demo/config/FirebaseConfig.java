b908b69f03bc84579c6da22b7e57ff41824bc9aa
awoltilahun <awoluawol@gmail.com>
2026-08-31 01:48:03 -0900

    Remove Firebase credentials from repo, add to gitignore

--- backend/src/main/java/com/techapp/demo/config/FirebaseConfig.java
+++ backend/src/main/java/com/techapp/demo/config/FirebaseConfig.java
@@ -12,21 +12,26 @@ import java.util.Arrays;
 public class FirebaseConfig {
 
     @Bean
-    public GoogleCredentials googleCredentials() throws IOException {
-        // Always use the service account file from resources
-        InputStream serviceAccount = getClass()
-                .getClassLoader()
-                .getResourceAsStream(
-                        "etechpro-1b433-firebase-adminsdk-fbsvc-289b460cd6.json");
+    public GoogleCredentials googleCredentials() {
+        try {
+            InputStream serviceAccount = getClass()
+                    .getClassLoader()
+                    .getResourceAsStream(
+                            "etechpro-1b433-firebase-adminsdk-fbsvc-289b460cd6.json");
 
-        if (serviceAccount == null) {
-            throw new IOException("Firebase service account file not found in resources");
+            if (serviceAccount == null) {
+                System.out.println("Firebase credentials file not found - notifications disabled");
+                return null;
+            }
+
+            return GoogleCredentials
+                    .fromStream(serviceAccount)
+                    .createScoped(Arrays.asList(
+                            "https://www.googleapis.com/auth/firebase.messaging"
+                    ));
+        } catch (Exception e) {
+            System.out.println("Firebase init failed: " + e.getMessage() + " - notifications disabled");
+            return null;
         }
-
-        return GoogleCredentials
-                .fromStream(serviceAccount)
-                .createScoped(Arrays.asList(
-                        "https://www.googleapis.com/auth/firebase.messaging"
-                ));
     }
 }
